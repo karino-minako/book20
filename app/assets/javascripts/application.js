c81d673978b54c64597fc5443adce21d3bf2b906
vagrant <vagrant@localhost.localdomain>
2020-05-21 08:23:19 +0000

    [Finish] 6-13

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+var title = "JavaScriptが使えました。";
+alert(title);
+
+$(document).ready(function () {
+	$("#theTarget").skippr({
+		transition : 'slide',
+		speed : 1000,
+		easing : 'easeOutQuart',
+		navType : 'block',
+		childrenElementType : 'div',
+		arrows : true,
+		autoPlay :true,
+		autoPlayDuration : 3000,
+		keyboardOnAlways : true,
+		hidePrevious : false
+	});
+});
